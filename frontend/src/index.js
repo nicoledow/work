acabf4980af1bb5d34d8b10f711844cc061bf332
Nicole Dow <nikki.dow71@gmail.com>
2019-11-04 13:07:25 -0700

    successfully rendering fetched workouts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 const BASEURL = 'http://localhost:3000/'
+const body = document.querySelector('body')
 
 document.addEventListener('DOMContentLoaded', getWorkouts());
 
 function getWorkouts(){
-    fetch(`${BASEURL}` + 'workoutsessions')
+    fetch(`${BASEURL}workouts`)
     //is this step necessary? Isn't Rails already rendering JSON?
     .then(response => response.json())
     .then(json => renderWorkouts(json))
@@ -14,18 +15,6 @@ function renderWorkouts(json){
         const div = document.createElement('div')
         div.className = 'workoutCard'
         div.id = workout.id
-
-        const h2 = document.createElement('h2')
-        h2.innerText = `${workout.title}`
-        div.appendChild(h2)
-
-        const h3 = document.createElement('h3')
-        h3.innerText = 'Focus:'
-        div.appendChild(h3)
-
-        const p1 = document.createElement('p')
-        p1.innerText = `${workout.focus}`
-        h3.appendChild(p1)
-
+        body.appendChild(div)
     })
 }
