9f40d8bf68aa8a0bd454f0ec24fc4e6640b20f43
Nicole Dow <nikki.dow71@gmail.com>
2019-11-05 09:26:27 -0700

    added belongs_to/has_many relationship with exercise/liftset

--- backend/work-api/app/controllers/liftsets_controller.rb
+++ backend/work-api/app/controllers/liftsets_controller.rb
@@ -2,7 +2,7 @@ class LiftsetsController < ApplicationController
 
   def show
     set = LiftSet.find_by_id(params[:id])
-    render json: set
+    render json: set, :include => {:exercise => {:only => [:name]}}
   end
 
 end
--- backend/work-api/app/models/lift_set.rb
+++ backend/work-api/app/models/lift_set.rb
@@ -1,3 +1,9 @@
 class LiftSet < ApplicationRecord
     belongs_to :workout
+    belongs_to :exercise
+
+    def get_exercise_name
+      exercise = Exercise.find_by_id(self.exercise_id)
+      exercise.name
+    end
 end
