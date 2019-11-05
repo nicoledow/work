4cecf895ee7871b7437abc1d64ccc0716ece8352
Nicole Dow <nikki.dow71@gmail.com>
2019-11-05 10:05:01 -0700

    reserialized json to include exercises

--- backend/work-api/app/controllers/workouts_controller.rb
+++ backend/work-api/app/controllers/workouts_controller.rb
@@ -7,6 +7,13 @@ class WorkoutsController < ApplicationController
 
     def show
       workout = Workout.find_by_id(params[:id])
-      render json: workout, :include => {:lift_sets => {:only => [:exercise_id, :reps, :weight, :goal]}}
+      #render json: workout, :include => {:lift_sets => {:only => [:exercise_id, :reps, :weight, :goal, :id, :exercise]}}
+      render json: workout, :include => {:exercises => 
+          {:only => [:name]},
+        :lift_sets => {
+          :except => [:created_at, :updated_at]
+        }
+        }
+
     end
 end
