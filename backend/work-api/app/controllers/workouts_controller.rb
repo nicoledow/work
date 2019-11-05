2a70409638310c3009193f958ea83b560a6a4d26
Nicole Dow <nikki.dow71@gmail.com>
2019-11-05 13:04:32 -0700

    added exercise to lift_set serialized json

--- backend/work-api/app/controllers/workouts_controller.rb
+++ backend/work-api/app/controllers/workouts_controller.rb
@@ -7,11 +7,11 @@ class WorkoutsController < ApplicationController
 
     def show
       workout = Workout.find_by_id(params[:id])
-      #render json: workout, :include => {:lift_sets => {:only => [:exercise_id, :reps, :weight, :goal, :id, :exercise]}}
       render json: workout, :include => {:exercises => 
           {:only => [:name]},
         :lift_sets => {
-          :except => [:created_at, :updated_at]
+          :except => [:created_at, :updated_at],
+          :include => [:exercise]
         }
         }
 
