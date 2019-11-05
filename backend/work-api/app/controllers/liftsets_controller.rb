9e1a6b744a5e3ea881ae540da9199be562d1a4e1
Nicole Dow <nikki.dow71@gmail.com>
2019-11-05 16:33:53 -0700

    added update route for lift_set

--- backend/work-api/app/controllers/liftsets_controller.rb
+++ backend/work-api/app/controllers/liftsets_controller.rb
@@ -5,4 +5,9 @@ class LiftsetsController < ApplicationController
     render json: set, :include => {:exercise => {:only => [:name]}}
   end
 
+  def update
+    set = LiftSet.find_by_id(params[:id])
+  end
+
+
 end
--- backend/work-api/config/routes.rb
+++ backend/work-api/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
  get '/workouts/:id', to: 'workouts#show'
 
  get '/liftsets/:id', to: 'liftsets#show'
+ patch '/liftsets/:id', to: 'liftsets#update'
 end
