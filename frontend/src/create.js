a8cafd8709bdd64815fa08f0dcbedb9e6e7bd5c4
Nicole Dow <nikki.dow71@gmail.com>
2019-11-06 11:21:09 -0700

    add default values to form

--- frontend/src/create.js
+++ frontend/src/create.js
@@ -3,23 +3,24 @@ const renderNewWorkoutForm = function() {
     headerDiv.appendChild(newDiv);
 
     let newForm = document.createElement('form');
-    newForm.setAttribute("method", "post");
-    newForm.setAttribute("action", `${BASE_URL}/workouts/new`)
     newDiv.appendChild(newForm);
 
     let titleInput = document.createElement('input');
     titleInput.setAttribute("type", "text");
     titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("value", "Workout Title");
     newForm.appendChild(titleInput);
 
     let dateInput = document.createElement('input');
     dateInput.setAttribute("type", "text");
     dateInput.setAttribute("name", "date");
+    dateInput.setAttribute("value", "Workout Date");
     newForm.appendChild(dateInput);
 
     let focusInput = document.createElement('input');
     focusInput.setAttribute("type", "text");
     focusInput.setAttribute("name", "focus");
+    focusInput.setAttribute("value", "Workout Focus");
     newForm.appendChild(focusInput)
 
     let submitButton = document.createElement('input');
@@ -27,5 +28,26 @@ const renderNewWorkoutForm = function() {
     submitButton.setAttribute("value", "Create");
     newForm.appendChild(submitButton);
 
-    //newForm.addEventListener('submit', renderNewLiftSetForm());
+    newForm.addEventListener('submit', function(event) {
+        submitNewWorkout(newForm);
+    }
+    )}
+
+function submitNewWorkout(form) {
+  fetch(`${BASE_URL}workouts/new`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify({
+          "title": form.querySelector('input[name="title"]').value,
+          "date": form.querySelector('input[name="date"]').value,
+          "focus": form.querySelector('input[name="focus"]').value
+      })
+  })
+  .then(response => response.json())
+  .then(function(object){
+      console.log(object);
+  })
 }
