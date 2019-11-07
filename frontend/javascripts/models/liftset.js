c8cc8fc0b0b6a8ceb6afa363471ec0aa4c067c78
Nicole Dow <nikki.dow71@gmail.com>
2019-11-07 12:29:40 -0700

    new liftset form generation now working

--- frontend/javascripts/models/liftset.js
+++ frontend/javascripts/models/liftset.js
@@ -5,8 +5,42 @@ class LiftSet {
       this.goal = goal;
   }
 
-  generateForm() {
-    alert('hit LiftSet.generateForm method');
+  //called with a dummy new LiftSet object and workoutDiv(to be reference for workout_id)
+  generateForm(set, workoutDiv) {
+    //alert('hit generateForm')
+    const setForm = document.createElement('form');
+    workoutDiv.appendChild(setForm);
+
+    const exerciseInput = document.createElement('input');
+    exerciseInput.setAttribute("type", "text");
+    exerciseInput.setAttribute("name", "exercise");
+    exerciseInput.setAttribute("value", "Exercise");
+    setForm.appendChild(exerciseInput);
+
+    const workoutIdInput = document.createElement('input');
+    workoutIdInput.setAttribute("type", "hidden");
+    workoutIdInput.setAttribute("value", `${workoutDiv.id}`);
+    setForm.appendChild(workoutIdInput);
+
+    const goalInput = document.createElement('input');
+    goalInput.setAttribute("type", "text");
+    goalInput.setAttribute("value", "Goal");
+    setForm.appendChild(goalInput);
+
+    const repsInput = document.createElement('input');
+    repsInput.setAttribute("type", "text");
+    goalInput.setAttribute("value", "Reps");
+    setForm.appendChild(repsInput);
+
+    const weightInput = document.createElement('input');
+    weightInput.setAttribute("type", "text");
+    weightInput.setAttribute("value", "Weight");
+    setForm.appendChild(weightInput);
+
+    const setSubmitButton = document.createElement('input');
+    setSubmitButton.setAttribute("type", "submit")
+    setSubmitButton.setAttribute("value", "Add");
+    setForm.appendChild(setSubmitButton);
   }
 
 }
