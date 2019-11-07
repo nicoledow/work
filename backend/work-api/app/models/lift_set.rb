b4b972a3b1393ad4798d951a8b4a444567fbd8d8
Nicole Dow <nikki.dow71@gmail.com>
2019-11-07 12:58:05 -0700

    narrow_by_workout(workout_id) method

--- backend/work-api/app/models/lift_set.rb
+++ backend/work-api/app/models/lift_set.rb
@@ -6,4 +6,8 @@ class LiftSet < ApplicationRecord
       exercise = Exercise.find_by_id(self.exercise_id)
       exercise.name
     end
+
+    def self.narrow_by_workout(workout_id)
+      LiftSet.all.select {|set| set.workout_id == workout_id}
+    end
 end
