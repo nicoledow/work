27909983c7e1ee0a271f86e551863acc918f058e
Nicole Dow <nikki.dow71@gmail.com>
2019-11-07 12:57:38 -0700

    configured liftsets#create action

--- backend/work-api/app/controllers/liftsets_controller.rb
+++ backend/work-api/app/controllers/liftsets_controller.rb
@@ -6,7 +6,19 @@ class LiftsetsController < ApplicationController
   end
 
   def create
+    lift_sets = LiftSet.narrow_by_workout(params[:workout_id])
 
+    if params[:exercise_id]
+      LiftSet.create(liftset_params)
+    else
+      new_set = LiftSet.new
+      new_set.exercise_id = Exercise.find_or_create_by(name: liftset_params[:exercise])
+      new_set.workout_id = liftset_params[:workout_id]
+      new_set.reps = liftset_params[:reps]
+      new_set.weight = liftset_params[:weight]
+      new_set.save
+    end
+    render json: lift_sets
   end
 
   def update
@@ -19,7 +31,7 @@ class LiftsetsController < ApplicationController
 
 private
   def liftset_params
-    params.permit(:exercise_id, :workout_id, :reps, :weight, :goal)
+    params.permit(:exercise_id, :workout_id, :reps, :weight, :goal, :exercise)
   end
 
 end
