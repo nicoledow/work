db0d2d908e16906e32a8c1c9834fa0d3fbd2231b
Nicole Dow <nikki.dow71@gmail.com>
2019-11-08 09:29:22 -0700

    remove alerts/rename functions

--- frontend/javascripts/models/liftset.js
+++ frontend/javascripts/models/liftset.js
@@ -5,4 +5,37 @@ class LiftSet {
     this.goal = goal;
   }
 
+  render(object) {
+    const workoutDiv = document.getElementById(object[0].workout_id);
+    console.log(object);
+
+    let newRow = document.createElement('tr');
+    workoutDiv.appendChild(newRow);
+    //form.appendChild(newRow);
+    
+    let newExerciseCell = document.createElement('td');
+    newExerciseCell.innerText = `${object[0].exercise.name}`;
+    newRow.appendChild(newExerciseCell);
+    
+    let newGoalCell = document.createElement('td');
+    newGoalCell.innerText = `${object[0].goal}`;
+    newRow.appendChild(newGoalCell);
+    
+    let newRepsCell = document.createElement('td');
+    let newRepsInput = document.createElement('input');
+    newRepsInput.setAttribute("type", "text");
+    newRepsInput.setAttribute("name", "actual-reps");
+    newRepsInput.setAttribute("value", `${object[0].reps}`);
+    newRepsCell.appendChild(newRepsInput);
+    newRow.appendChild(newRepsCell);
+    
+    let newWeightCell = document.createElement('td');
+    let newWeightInput = document.createElement('input');
+    newWeightInput.setAttribute("type", "text");
+    newWeightInput.setAttribute("name", "actual-weight");
+    newWeightInput.setAttribute("value", `${object[0].weight}`);
+    newWeightCell.appendChild(newWeightInput);
+    newRow.appendChild(newWeightCell);
+  }
+
 }
--- frontend/javascripts/services/app.js
+++ frontend/javascripts/services/app.js
@@ -73,40 +73,6 @@ class App {
           app.updateLiftSets(button);
         });
       }
-
-    addSetHTMLToDiv(object) {
-        const workoutDiv = document.getElementById(object[0].workout_id);
-        console.log('in liftset.addSetHTMLToDiv func');
-        console.log(object);
-
-        let newRow = document.createElement('tr');
-        workoutDiv.appendChild(newRow);
-        //form.appendChild(newRow);
-        
-        let newExerciseCell = document.createElement('td');
-        newExerciseCell.innerText = `${object[0].exercise.name}`;
-        newRow.appendChild(newExerciseCell);
-        
-        let newGoalCell = document.createElement('td');
-        newGoalCell.innerText = `${object[0].goal}`;
-        newRow.appendChild(newGoalCell);
-        
-        let newRepsCell = document.createElement('td');
-        let newRepsInput = document.createElement('input');
-        newRepsInput.setAttribute("type", "text");
-        newRepsInput.setAttribute("name", "actual-reps");
-        newRepsInput.setAttribute("value", `${object[0].reps}`);
-        newRepsCell.appendChild(newRepsInput);
-        newRow.appendChild(newRepsCell);
-        
-        let newWeightCell = document.createElement('td');
-        let newWeightInput = document.createElement('input');
-        newWeightInput.setAttribute("type", "text");
-        newWeightInput.setAttribute("name", "actual-weight");
-        newWeightInput.setAttribute("value", `${object[0].weight}`);
-        newWeightCell.appendChild(newWeightInput);
-        newRow.appendChild(newWeightCell);
-    }
         
           //called with  workoutDiv(to be reference for workout_id)
     generateLiftForm(workoutDiv) {
