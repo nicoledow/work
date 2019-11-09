92f744c529937bb7fe99bb7b1327c69643cec8b8
Nicole Dow <nikki.dow71@gmail.com>
2019-11-09 14:36:26 -0700

    edit json serializing

--- backend/work-api/app/controllers/exercises_controller.rb
+++ backend/work-api/app/controllers/exercises_controller.rb
@@ -4,4 +4,11 @@ class ExercisesController < ApplicationController
       exercises = Exercise.all
       render json: exercises, :only => [:name, :id]
     end
+
+    def show
+      exercise = Exercise.find_by_id(params[:id])
+      sets = exercise.lift_sets
+      render json: sets, :include => {:exercise => 
+        {:only => [:name, :id]}}
+    end
 end
