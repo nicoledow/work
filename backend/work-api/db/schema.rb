5463107ba2f4c9dabc9051c56c844e4b22544f9d
Nicole Dow <nikki.dow71@gmail.com>
2019-11-12 10:47:33 -0700

    add 'liked' column to workouts

--- backend/work-api/db/schema.rb
+++ backend/work-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_211939) do
+ActiveRecord::Schema.define(version: 2019_11_12_172957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_211939) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed", default: false
+    t.boolean "liked"
   end
 
 end
